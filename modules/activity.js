43b96f969937c011f8be2d15ba0a289cd08a8e29
clear-chenxiaobin <chenxiaobin@cleartv.cn>
2016-08-03 09:59:16 +0800

    2016/08/03 new

---
+++ modules/activity.js
@@ -0,0 +1,80 @@
+/**
+ * Created by 83471 on 2016/8/2.
+ */
+'use strict';
+
+angular.module('app.activity', ['app.resource'])
+    .service('ActivityManager',['ResourceManager', function (ResourceManager) {
+        var activityStack = [];
+
+        function Activity(id) {
+            this.templateUrl = 'partials/' + id + '.html';
+            this._hide = false;
+        }
+
+        Activity.prototype.finish = function () {
+            activityStack.pop();
+        };
+
+        Activity.prototype.initialize = function (scope) {
+            this._scope = scope;
+        };
+
+        Activity.prototype.hide = function () {
+            this._hide = true;
+        };
+
+        Activity.prototype.show = function () {
+            this._hide = false;
+        };
+
+        Activity.prototype.isHide = function () {
+            return this._hide;
+        };
+
+
+        Activity.prototype.keyUp = function (keyCode) {
+            typeof(this._keyUp_callback_) === 'function' && this._keyUp_callback_(keyCode);
+        };
+
+        Activity.prototype.keyDown = function (keyCode) {
+            typeof(this._keyDown_callback_) === 'function' && this._keyDown_callback_(keyCode);
+        };
+
+        Activity.prototype.onKeyUp = function (callback) {
+            this._keyUp_callback_ = callback;
+        };
+
+        Activity.prototype.onKeyDown = function (callback) {
+            this._keyDown_callback_ = callback;
+        };
+
+        Activity.prototype.loadI18NResource = function (callback) {
+            callback(ResourceManager.getI18NResource());
+            this._scope.$on('locale.change', function (ev, keyCode) {
+                callback(ResourceManager.getI18NResource());
+            });
+        };
+
+        this.go = function (id, stackIndex) {
+            var nPops = activityStack.length - stackIndex;
+            for (var i = 0; i < nPops; i++) {
+                activityStack.pop();
+            }
+            this.startActivity(id);
+        };
+
+        this.startActivity = function (id) {
+            var activity = new Activity(id);
+            activityStack.push(activity);
+        };
+
+        this.getActiveActivity = function () {
+            return activityStack[activityStack.length - 1];
+        };
+
+        this.getActivityStack = function () {
+            return activityStack;
+        };
+
+    }]);
---
+++ modules/welcome.js
@@ -0,0 +1,49 @@
+'use strict';
+
+angular.module('app.welcome', [])
+    .controller('WelcomeController', ['$scope', 'ResourceManager', 'ActivityManager', 'COMMON_KEYS', function ($scope, ResourceManager, ActivityManager, COMMON_KEYS) {
+        var activity = ActivityManager.getActiveActivity();
+        activity.initialize($scope);
+
+        activity.loadI18NResource(function (res) {
+            var i18nText;
+            if(ResourceManager.getLocale()){
+                i18nText  = ResourceManager.getLocale();
+            }else{
+                $scope.language = 'zh-CN';
+                ResourceManager.setLocale($scope.language);
+                i18nText = ResourceManager.getLocale();
+            }
+            $scope.guestNameText = i18nText.index.guestName;
+            $scope.guestName = res.getString("guest_name");
+            //$scope.welcomeText = '欢迎您来到东方滨江大酒店';
+            //$scope.subWelcomeText = [
+            //    '您好！衷心欢迎阁下光临东方滨江大酒店',
+            //    '我们愿始终如一的为阁下提供优质的产品和服务',
+            //    '使每一位宾客宾至如归，令阁下倍感尊崇之礼遇']
+            //    .join('\n');
+            $scope.welcomeText = i18nText.welcome.welcome_text;
+            $scope.roomNumber = i18nText.index.roomNumber + window.localStorage.room;
+            $scope.press1 = i18nText.welcome.press1;
+            $scope.press2 = i18nText.welcome.press2;
+        });
+        var languages = ['zh-CN', 'en-US'],
+            languageIndex = 0;
+
+        activity.onKeyDown(function (keyCode) {
+            switch (keyCode) {
+                case COMMON_KEYS.KEY_LEFT:
+                case COMMON_KEYS.KEY_RIGHT:
+                    languageIndex ^= 1;
+                    $scope.language = languages[languageIndex];
+                    ResourceManager.setLocale($scope.language);
+                    break;
+                case COMMON_KEYS.KEY_ENTER:
+                    ActivityManager.startActivity('menu');
+                    break;
+                case COMMON_KEYS.KEY_UP:
+                    ActivityManager.startActivity('test');
+                    break;
+            }
+        });
+    }]);
