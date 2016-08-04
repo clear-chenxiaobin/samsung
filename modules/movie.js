9902c58ff8fd8e75bb2a371db81ab092d97347ba
kunmingren <834719922@qq.com>
2016-08-04 17:36:26 +0800

    movie动画

--- modules/movie.js
+++ modules/movie.js
@@ -14,18 +14,93 @@ angular.module('app.movie',[])
             //$scope.press1 = i18nText.welcome.press1;
             //$scope.press2 = i18nText.welcome.press2;
         });
+        $scope.typeIndex = 0;
+        $scope.movieIndex = 0;
         $scope.movie = [
             {
                 type:'动作',
-                img:'',
-                bgImg:''
+                list:[
+                    {
+                        name:'火星救援',
+                        img:'assets/images/movie1.png',
+                        bgimg:'assets/images/banner1.png'
+                    },
+                    {
+                        name:'侏罗纪世界',
+                        img:'assets/images/movie2.png',
+                        bgimg:'assets/images/banner2.png'
+                    },
+                    {
+                        name:'海底总动员1',
+                        img:'assets/images/movie3.png',
+                        bgimg:'assets/images/banner3.png'
+                    }
+                ]
+            },
+            {
+                type:'科幻',
+                list:[
+                    {
+                        name:'海底总动员1',
+                        img:'assets/images/movie3.png',
+                        bgimg:'assets/images/banner3.png'
+                    },
+                    {
+                        name:'海底总动员1',
+                        img:'assets/images/movie3.png',
+                        bgimg:'assets/images/banner3.png'
+                    },
+                    {
+                        name:'侏罗纪世界',
+                        img:'assets/images/movie2.png',
+                        bgimg:'assets/images/banner2.png'
+                    },
+                    {
+                        name:'火星救援',
+                        img:'assets/images/movie1.png',
+                        bgimg:'assets/images/banner1.png'
+                    }
+                ]
+            },
+            {
+                type:'爱情',
+                list:[
+                    {
+                        name:'火星救援',
+                        img:'assets/images/movie1.png',
+                        bgimg:'assets/images/banner1.png'
+                    },
+                    {
+                        name:'侏罗纪世界',
+                        img:'assets/images/movie2.png',
+                        bgimg:'assets/images/banner2.png'
+                    },
+                    {
+                        name:'侏罗纪世界',
+                        img:'assets/images/movie2.png',
+                        bgimg:'assets/images/banner2.png'
+                    },
+                    {
+                        name:'海底总动员1',
+                        img:'assets/images/movie3.png',
+                        bgimg:'assets/images/banner3.png'
+                    },
+                    {
+                        name:'海底总动员1',
+                        img:'assets/images/movie3.png',
+                        bgimg:'assets/images/banner3.png'
+                    }
+                ]
             }
         ]
+
+        var typeID = 0;
         var movieID = 1;
         ActivityManager.showLoading();
         choseMovie(movieID);
         ActivityManager.hideLoading(2000);
         function changeMovieType(typeID){
+            $scope.movieList = $scope.movie[typeID].list;
 
         }
         function choseMovie(movieID){
@@ -35,6 +110,66 @@ angular.module('app.movie',[])
             var URLStr = 'url('+ imageURL+')';
             movieDetail.style.backgroundImage = URLStr;
         }
+
+        function rotateUp(num){
+            if(num == 0) {
+                var number = num;
+                var target = document.getElementById('type' + number).children;
+                for(var i=0;i<target.length;i++){
+                    activity.transform(target[i],"rotateX(0deg)");
+                    activity.removeClass(target[i], 'opacityReduce');
+                    activity.addClass(target[i], 'opacityAdd');
+                    target[i].style.top = '0px';
+                }
+            }else{
+                var number = num;
+                var target = document.getElementById('type' + number).children;
+                for(var i=0;i<target.length;i++){
+                    activity.transform(target[i],"rotateX(0deg)");
+                    activity.removeClass(target[i], 'opacityReduce');
+                    activity.addClass(target[i], 'opacityAdd');
+                    target[i].style.top = '0px';
+                }
+                var number1 = num+1;
+                var target1 = document.getElementById('type'+ number1).children;
+                for(var i=0;i<target1.length;i++) {
+                    activity.transform(target1[i], "rotateX(90deg)");
+                    activity.addClass(target1[i], 'opacityReduce');
+                    target1[i].style.top = '-115px';
+                }
+            }
+        }
+
+
+        function rotateDown(num){
+            if(num==-1){
+                var target = document.getElementsByClassName('rotate_img');
+                for(var i=0;i<target.length;i++){
+                    activity.transform(target[i],"rotateX(-90deg)");
+                    activity.addClass(target[i], 'opacityReduce');
+                    target[i].style.top = '100px';
+                }
+            }else{
+                var number1 = num+1;
+                var target1 = document.getElementById('type'+ number1).children;
+                for(var i=0;i<target1.length;i++) {
+                    activity.transform(target1[i], "rotateX(-90deg)");
+                    activity.addClass(target1[i], 'opacityReduce');
+                    target1[i].style.top = '100px';
+                }
+                var number = num;
+                var target = document.getElementById('type' + number).children;
+                for(var i=0;i<target.length;i++){
+                    activity.transform(target[i],"rotateX(0deg)");
+                    activity.removeClass(target[i], 'opacityReduce');
+                    activity.addClass(target[i], 'opacityAdd');
+                    target[i].style.top = '0px';
+                }
+            }
+        }
+
+        //rotateDown(-1);
+        //rotateUp(0);
 
         activity.onKeyDown(function (keyCode) {
             switch (keyCode) {
@@ -56,10 +191,16 @@ angular.module('app.movie',[])
                     ActivityManager.startActivity('menu');
                     break;
                 case COMMON_KEYS.KEY_UP:
-                    ActivityManager.hideLoading(3000);
+                    if ($scope.typeIndex < $scope.movie.length) {
+                        $scope.typeIndex++;
+                    }
+                    rotateUp($scope.typeIndex);
                     break;
                 case COMMON_KEYS.KEY_DOWN:
-                    ActivityManager.showLoading();
+                    if ($scope.typeIndex > 0) {
+                        $scope.typeIndex--;
+                    }
+                    rotateDown($scope.typeIndex);
                     break;
             }
         });
